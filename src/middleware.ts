05e13df3ff19f2eb13e9ca64ecb5084dd4a2ee9f
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-10-01 14:25:27 -0500

    feat: restrict pro url for non-pro folks

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-server-import-in-page */
 import { NextRequest, NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
+import { get } from './lib/redis';
 
 export default async function middleware(req: NextRequest) {
 	const token = await getToken({ req });
@@ -12,11 +13,19 @@ export default async function middleware(req: NextRequest) {
 		return NextResponse.redirect(req.nextUrl);
 	}
 
-	if (token) return NextResponse.next();
-	else req.nextUrl.pathname = '/auth';
+	if (token) {
+		if (url === '/pro') {
+			const data = await get(token?.email as string);
+			if (!data?.stripeId) {
+				req.nextUrl.pathname = '/';
+				return NextResponse.redirect(req.nextUrl);
+			}
+		}
+		return NextResponse.next();
+	} else req.nextUrl.pathname = '/auth';
 	return NextResponse.rewrite(req.nextUrl);
 }
 
 export const config = {
-	matcher: ['/', '/auth'],
+	matcher: ['/', '/auth', '/pro'],
 };
