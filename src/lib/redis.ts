62f4f4a8d9160e92d736ae2d12edd1aa7aafa704
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-10-01 13:10:30 -0500

    feat: crud operations to redis

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -4,3 +4,15 @@ const redis = new Redis({
 	url: process.env.UPSTASH_URL as string,
 	token: process.env.UPSTASH_TOKEN as string,
 });
+
+export const create = async (key: string) => {
+	await redis.set(key, 0);
+};
+
+export const get = async (key: string) => {
+	return (await redis.get(key)) as number | null;
+};
+
+export const set = async (key: string, value: number) => {
+	await redis.set(key, value);
+};
