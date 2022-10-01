562fe7d54375db0925502d7cb0764336b0bf04bd
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-10-01 11:44:25 -0500

    feat: using tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,15 @@
 import { trpc } from '@/lib/trpc';
 import type { NextPage } from 'next';
-import Head from 'next/head';
 import { useRouter } from 'next/router';
-import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
 	const { query } = useRouter();
 	const { data } = trpc.useQuery(['user.getUserById', { id: (query.id as string) || '1' }]);
 
 	return (
-		<div className={styles.container}>
-			<Head>
-				<title>Next.js TRPC Template</title>
-				<meta name="description" content="TRPC Template for Next.js" />
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
+		<div className="h-screen w-screen">
 			<div>
-				<h1>{data?.user?.name}</h1>
+				<h1 className="">{data?.user?.name}</h1>
 				<p>{data?.user?.email}</p>
 			</div>
 		</div>
