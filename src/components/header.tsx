092b6b006c82536c20aad0108cdf89e54995b5fd
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-10-01 16:10:23 -0500

    feat: add pro tag to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,14 @@
+import { trpc } from '@/lib/trpc';
+import Pro from './pro';
+
 export default function Header() {
+	const { data, isLoading } = trpc.useQuery(['user.data']);
+
 	return (
 		<div className="flex py-8 shadow w-full h-max justify-center">
-			<h1 className="font-bold text-3xl italic text-purple-600">Cleek</h1>
+			<h1 className="font-bold text-3xl italic text-purple-600">
+				Cleek {data?.stripeId && <Pro />}
+			</h1>
 		</div>
 	);
 }
