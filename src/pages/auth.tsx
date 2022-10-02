476c7f60f1f11997fd813d557fbf8c4988a92a8f
Garrett Tolbert <garrett1.tolbert@gmail.com>
2022-10-02 12:58:31 -0500

    refactor: remove github auth

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -4,13 +4,13 @@ import { signIn } from 'next-auth/react';
 const Auth: NextPage = () => {
 	return (
 		<div className="h-screen w-screen flex flex-col space-y-4 items-center justify-center">
-			<button
+			{/* <button
 				type="button"
 				onClick={() => signIn('github')}
 				className="inline-flex w-max items-center px-5 py-2 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-black hover:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black"
 			>
 				Sign in with Github
-			</button>
+			</button> */}
 			<button
 				type="button"
 				onClick={() => signIn('google')}
